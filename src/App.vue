a874bb95d700150a61e6500fab424f4abd29f4a9
Mathew Morse <mrm1779@gmail.com>
2021-12-21 14:43:05 -0500

    User add sticky note, connect and save to the database, get all sticky notes from the API

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
       <sticky-note v-for="sticky in stickyNotes" :key="sticky" :test="sticky.note"></sticky-note>
       {{ info }}
     </div>
+
+    <button id="showModal" @click="showModal = true">Show Modal</button>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <button id="closeModal" @click="showModal = false">Close Modal</button>
+      <span>What would you like to share?</span>
+      <textarea v-model="message"></textarea>
+      <button id="submitModal" @click="submitModal(message)">Submit</button>
+    </modal>
   </div>
 </template>
 
@@ -18,25 +27,42 @@
     },
     data: function() {
       return {
-        stickyNotes: [
-          {note: 'Lorem ipsum 1'},
-          {note: 'Lorem ipsum 2'},
-          {note: 'Lorem ipsum 3'}
-        ],
-        info: []
+        stickyNotes: [],
+        showModal: false,
+        isLoading: false
       }
     },
     created() {
-      axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => {
-        // handle success
-        this.info = response.data
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
+      this.getAllSticky().then(() => {});
+    },
+    methods: {
+      submitModal(stickyMessage) {
+        console.log(stickyMessage);
+        this.isLoading = true;
+
+        return axios.post('http://localhost:3000/stickyNoteAdd', {
+          note: stickyMessage
+        }).then(() => {
+          this.isLoading = false;
+          this.showModal = false;
+          this.getAllSticky();
+        }).catch(err => {
+          console.error(err);
+          this.isLoading = false;
+        });
+      },
+      getAllSticky: function() {
+        axios
+        .get('http://localhost:3000/getAllSticky')
+        .then(response => {
+          // handle success
+          this.stickyNotes = response.data;
+        })
+        .catch(function (error) {
+          // handle error
+          console.error(error);
+        });
+      }
     }
   }
 </script>
